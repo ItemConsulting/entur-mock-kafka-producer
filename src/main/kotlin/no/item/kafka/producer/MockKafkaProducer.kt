0b8b8bf0f49e73d5fbecf54daff8c2af4f4a9ebb
Åsmund Bryne Retterstøl <retterstol@gmail.com>
2020-06-29 09:29:08 +0200

    Make a test-code

--- src/main/kotlin/no/item/kafka/producer/MockKafkaProducer.kt
+++ src/main/kotlin/no/item/kafka/producer/MockKafkaProducer.kt
@@ -2,9 +2,16 @@ package no.item.kafka.producer
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import java.lang.Thread.sleep
 
 fun main(args: Array<String>) {
-  MockKafkaProducer().run()
+  //MockKafkaProducer().run()
+  var i = 1
+  while(true){
+    println("hei " + i)
+    i++
+    sleep(500)
+  }
 }
 
 class MockKafkaProducer {
